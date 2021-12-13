c9530722ddfc5191b491cdb73d6778c0bb97db56
SteveMiller7 <info@millercreativedesign.com>
2021-12-13 11:27:05 +0000

    Set up tructure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
 
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function inrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
